ef4ad358d90635e54daa98c9993f8da58889fc10
hjkwon0814 <hjkwon0814@naver.com>
2022-12-08 14:58:06 +0900

    완료

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response  # 서버 구현을 위한 Flask 객체 import
+from flask import Flask, make_response, request  # 서버 구현을 위한 Flask 객체 import
 from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
 from surprise import SVD
 from surprise import Dataset
@@ -9,10 +9,13 @@ app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션
 api = Api(app)  # Flask 객체에 Api 객체 등록
 CORS(app)
 
-@api.route('/recommand')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
+@api.route('/recommand', methods=['GET', 'POST'])  # 데코레이터 이용, '/hello' 경로에 클래스 등록
 class HelloWorld(Resource):
-    def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
+    def post(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
         # 내장 데이터인 무비렌즈 데이터 로드하고 학습/테스트 데이터로 분리
+
+        params = request.get_json()
+        id = params['id']
         data = Dataset.load_builtin('ml-100k', prompt=False)
         train, test = train_test_split(data, test_size=0.25,
                               random_state=42)
@@ -23,10 +26,17 @@ class HelloWorld(Resource):
 
         prediction = algo.test(test)
 
-        print('prediction 결과값 5개 미리보기')
-        print(prediction[:5])
+        recommandList = []
 
-        return {"items": prediction}
+        for i in prediction:
+            if int(i.uid) == id :
+                recommandList.append({"uid": i.uid, "iid" : i.iid, "est" : str(i.est)})
+
+
+        print(recommandList)
+        # print(prediction)
+
+        return {"items": recommandList}
 
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=8080)
