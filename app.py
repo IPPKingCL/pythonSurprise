6741a244e4beed8a74f37c0eb12d875b5e59e535
chobkyu <qudqud97@naver.com>
2022-12-08 14:21:14 +0900

    디비 연동법

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ from surprise import SVD
 from surprise import Dataset
 from surprise.model_selection import train_test_split
 from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+import pymysql
 
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 api = Api(app)  # Flask 객체에 Api 객체 등록
 CORS(app)
 
+     
+     
 @api.route('/recommand')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
 class HelloWorld(Resource):
     def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
@@ -22,7 +27,7 @@ class HelloWorld(Resource):
         train, test = train_test_split(data, test_size=0.25,
                               random_state=42)
 
-
+        
         algo = SVD()
         algo.fit(train)
 
@@ -32,6 +37,20 @@ class HelloWorld(Resource):
         print(prediction[:5])
 
         return {"items": prediction}
+
+        #디비 연동하는 법입니다
+        #db = pymysql.connect(host=os.environ.get("FLASK_HOST"),user = os.environ.get("FLASK_USER"), db = os.environ.get("FLASK_DB"), password=os.environ.get("FLASK_PW") , charset=os.environ.get("FLASK_CHARSET"))
+        #curs = db.cursor()
+
+        #sql = "select * from user"
+
+        #curs.execute(sql)
+        #rows = curs.fetchall()
+        #print(rows)
+
+        #db.commit()
+        #db.close()
+       
 
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=8080)
