bf58215e0c8e735478d93fd5c20eab98dfa9922a
hjkwon0814 <hjkwon0814@naver.com>
2022-12-08 11:23:10 +0900

    header

--- server.py
+++ server.py
@@ -1,17 +1,22 @@
-from flask import Flask  # 서버 구현을 위한 Flask 객체 import
+from flask import Flask, make_response  # 서버 구현을 위한 Flask 객체 import
 from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
 from surprise import SVD
 from surprise import Dataset
 from surprise.model_selection import train_test_split
+from flask_cors import CORS
 
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 api = Api(app)  # Flask 객체에 Api 객체 등록
-
+CORS(app)
 
 @api.route('/recommand')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
 class HelloWorld(Resource):
     def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
         
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add('Access-Control-Allow-Headers', "*")
+        response.headers.add('Access-Control-Allow-Methods', "*")
         # 내장 데이터인 무비렌즈 데이터 로드하고 학습/테스트 데이터로 분리
         data = Dataset.load_builtin('ml-100k', prompt=False)
         train, test = train_test_split(data, test_size=0.25,
